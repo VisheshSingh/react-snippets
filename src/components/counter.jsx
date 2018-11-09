cee2d9125925a7b3bf8d0cec929be8c6f282da40
Vishesh <visheshsingh16@gmail.com>
2018-11-09 16:40:34 -0500

    Binding event handlers to 'this'

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,25 +5,18 @@ class Counter extends Component {
     count: 0,
     tags: ["tag1", "tag2", "tag3"]
   };
-
+  constructor() {
+    super();
+    // console.log("Constructor", this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
   handleIncrement() {
     console.log("Increment", this);
-  }
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
   }
 
   render() {
     return (
       <React.Fragment>
-        <div>{this.renderTags()}</div>
         <button onClick={this.handleIncrement} className="btn btn-secondary">
           Increment
         </button>
