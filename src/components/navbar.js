64a160bf9c3880d6043ee21579c9ddfd194b4d20
Vishesh <visheshsingh16@gmail.com>
2018-11-09 18:31:23 -0500

    Destructuring arguments

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Navbar = props => {
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="nav navbar-dark bg-dark">
       <a href="/" className="navbar-brand ">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
