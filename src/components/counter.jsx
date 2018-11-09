b92e11572ad8e96532da13b5d19d8de064c7e10d
Vishesh <visheshsingh16@gmail.com>
2018-11-09 17:15:28 -0500

    Passing data from parent to child thru props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,37 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  handleIncrement = e => {
-    console.log(e);
+  handleIncrement = () => {
     this.setState({
-      count: this.state.count + 1
+      value: this.state.value + 1
     });
   };
 
-  formatCount() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+  formatvalue() {
+    return this.state.value === 0 ? "Zero" : this.state.value;
   }
 
   renderClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   render() {
     return (
       <div style={{ margin: "10px" }}>
-        <span className={this.renderClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => {
-            this.handleIncrement("blah blah blah");
-          }}
-          className="btn btn-secondary"
-        >
+        <span className={this.renderClasses()}>{this.formatvalue()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary">
           Increment
         </button>
       </div>
