384fddfef25225dddbe37534e09d4200f889e2b9
Vishesh <visheshsingh16@gmail.com>
2018-11-09 18:21:33 -0500

    Lifting the state up and passing data via props

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="nav navbar-dark bg-dark">
+        <a href="/" className="navbar-brand ">
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
